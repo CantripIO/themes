a1d9634a984f2767e234523f3ed52b77ffef36c4
Jeff Kilroy <jeffkilroy@gmail.com>
2020-12-27 06:21:17 -0500

    post page

--- resources/js/components/layout/single-page-post.vue
+++ resources/js/components/layout/single-page-post.vue
@@ -17,6 +17,32 @@
                     <li>Christopher Perkins, James Haeck, James Introcaso, Adam Lee, Matthew Sernett (September 2018). Waterdeep: Dragon Heist. Edited by Jeremy Crawford. (Wizards of the Coast), chap. Appendix B, p. 196. ISBN 978-0-7869-6625-7.</li>
                     <li> Eric L. Boyd (June 2005). City of Splendors: Waterdeep. (Wizards of the Coast), p. 69. ISBN 0-7869-3693-2.</li>
                 </ul>
+                <h1>Faerûn Geography</h1>
+
+                <p><strong>The continent included terrain that was as varied as any other.</strong> Besides the exterior coastline to the <em>west and south</em>, the most dominant feature on the continent was the <code>Sea of Fallen Stars</code>. This was an irregular inland sea that kept the interior lands fertile, connected the west and east regions of Faerûn and <a href="#">served as a major trade route for many of the bordering nations</a>.</p>
+
+                <h2>Interior</h2>
+
+                <ol>
+                    <li>Cormyr: A feudal nation of humans living on former elven and dragon territory.</li>
+                    <li>The Dragon Coast: The southern coast of the Sea of Fallen Stars was a den for pirates, thieves and members of the regions' more unseemly organizations.</li>
+                </ol>
+
+                <blockquote><p>With the exception of the Shining Plains, these lands lay along the irregular coastline of the western Sea of Fallen Stars. In the north, the Dragonmere arm of the sea extended far to the west, ending close to the Western Heartlands. To the south, the Vilhon Reach formed a second arm leading to the southwest.</p></blockquote>
+
+                <h3>Regions</h3>
+
+                <ul>
+                    <li>Cormyr: This was a prosperous, civilized realm that lay at the western end of the Sea of Fallen Stars, to the south of the Anauroch desert and the Dalelands</li>
+                    <li>Sembia: On the northwest shore of the Sea of Fallen Stars was this prosperous and ambitious nation of merchants and traders, that served as a vassal state of the returned Netheril.</li>
+                </ul>
+
+                <p>You can use the code below to embed our tabletop rpg software:</p>
+                <pre><code>
+if(player.alignment == 'chaotic-neutral'){
+    player.attack(randomNPC);
+}
+</code></pre>
             </div>
             <div class="single-post-footer-byline">
                 <div class="single-post-author">LhynardBot</div>
